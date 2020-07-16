fcd7cf1fbff57be5c35c5394c3f0f67316ce1a80
norival marson junior <jrmarson.nmj@gmail.com>
2020-07-15 21:58:18 -0300

    Projeto criado

---
+++ src/main/java/com/norival/cursomcvirtualtres/CursomcvirtualtresApplication.java
@@ -0,0 +1,13 @@
+package com.norival.cursomcvirtualtres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcvirtualtresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcvirtualtresApplication.class, args);
+	}
+
+}
