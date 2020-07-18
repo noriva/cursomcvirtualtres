b454a10711b8c79f752ffbf7601266864ddee83d
norival marson junior <jrmarson.nmj@gmail.com>
2020-07-17 22:03:27 -0300

    criando repository e service para categoria

---
+++ src/main/java/com/norival/cursomcvirtualtres/repositories/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.norival.cursomcvirtualtres.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.norival.cursomcvirtualtres.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+	//Operações de acesso a dados como busca , salvar , alterar , delatar
+	
+	
+	
+}
